68752bda5d39452533965dbeebe9a9fa59922019
Lains <lainsce@airmail.cc>
2018-01-10 04:42:36 -0200

    * - flatpak friendly

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,11 +4,7 @@ import subprocess
 import platform
 
 schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
-icondir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir], shell=False)
-
-    print('Rebuilding desktop icons cache...')
-    subprocess.call(['update-icon-caches', icondir], shell=False)
